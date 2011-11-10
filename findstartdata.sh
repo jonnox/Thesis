c13427f907a05a37fa9c88536cb0226cc759f3ba
Jon Elliott win <jonnox@gmail.com>
2011-11-10 07:35:57 -0500

    sh script + images

---
+++ findstartdata.sh
@@ -0,0 +1,11 @@
+bx=47
+by=35
+rx=47
+ry=122
+gx=48
+gy=207
+
+java test.ImageCli ./startdata.jpg ${rx} ${ry} 20
+java test.ImageCli ./startdata.jpg ${gx} ${gy} 20
+java test.ImageCli ./startdata.jpg ${bx} ${by} 20
+java test.ImageCli ./startdatadash.jpg ${bx} ${by} 20
